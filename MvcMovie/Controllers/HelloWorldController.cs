768a2ffc95df1bf0fee5d4d0b3cb8819661f981b
Siphesihle Dube <Siphesihle Dube>
2017-07-02 22:23:42 +0200

    Added AspNetCore MVC Project

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index() {
+            return View();
+        }
+
+        //public string Index() {
+        //    return "This is my default action...";
+        //}
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome() {
+            return "This is the Welcome action method";
+        }
+
+        public string Data(string name, int numTimes = 1 ) {
+            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is: {numTimes}");
+        }
+
+        public string MoreData(string name, int ID = 1) {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
