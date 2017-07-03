fd2842a90fdb6d761df64e76ffe5dab290e6c193
Siphesihle Dube <Siphesihle Dube>
2017-07-03 21:29:32 +0200

    Added a movie model to the project

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,26 +5,15 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
         public IActionResult Index() {
             return View();
         }
 
-        //public string Index() {
-        //    return "This is my default action...";
-        //}
+        public IActionResult Welcome(string name, int numTimes) {
+            ViewData["name"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
-        //GET: /HelloWorld/Welcome/
-        public string Welcome() {
-            return "This is the Welcome action method";
-        }
-
-        public string Data(string name, int numTimes = 1 ) {
-            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is: {numTimes}");
-        }
-
-        public string MoreData(string name, int ID = 1) {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            return View();
         }
 
     }
